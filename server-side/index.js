8cc0c7e04b90e2a3a2d42b5353c6e3846d06ed89
Abhishek Gaur <abhibhardwaj@Abhis-MacBook-Air.local>
2025-07-18 19:51:48 +0530

    done

--- server-side/index.js
+++ server-side/index.js
@@ -5,7 +5,15 @@ const mongoose = require("mongoose")
 const app = express(); 
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const cors = require("cors")
 
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 
 app.use(express.json());
