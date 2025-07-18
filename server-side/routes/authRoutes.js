e1665683206769fcbc1820106f6c2ac7d053f0b4
Munish Sharma <munishs274@gmail.com>
2025-07-18 21:17:41 +0530

    all files

--- server-side/routes/authRoutes.js
+++ server-side/routes/authRoutes.js
@@ -1,14 +1,3 @@
-
-
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.post('/update', authMiddleware, userController.update);
-
 module.exports = router;
 
     const express = require('express');
